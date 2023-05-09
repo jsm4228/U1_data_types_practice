fa9016a1e1ed630dc3dac829fd77154b63cfd92b
Josh Mathew <jsmathew41@gmail.com>
2023-05-09 13:23:12 -0500

    initial commit

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
  * Declare a variable called myFavoriteSong on one line and console.log the value
  * Then, using your Literals, console.log "my favorite song is..." with the song's value
  */
+let myFavoriteSong = 'White Ferrari'
+console.log(`my favorite song is ${myFavoriteSong}`)
 
 /*
  * Prompt 2:
